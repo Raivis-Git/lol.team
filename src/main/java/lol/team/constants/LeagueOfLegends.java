88ad15afef5932992d27ac901d6f3a90a9315cd8
Raivis Treikals <raivis.treikals@banknote.lv>
2021-03-15 13:12:52 +0200

    1

--- src/main/java/lol/team/constants/LeagueOfLegends.java
+++ src/main/java/lol/team/constants/LeagueOfLegends.java
@@ -1,5 +1,5 @@
 package lol.team.constants;
 
 public class LeagueOfLegends {
-    public static final String API_KEY = "RGAPI-92f9a53e-73e7-4698-b77c-53727e5fe9b9";
+    public static final String API_KEY = "";
 }
