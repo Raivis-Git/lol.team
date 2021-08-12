8305583621d642b895849fd77d6776b7530ef3df
Raivis Treikals <raivis.treikals@banknote.lv>
2021-08-12 21:10:05 +0300

    Adjustments for maven and docker, added frontend project, project refactor

--- backend/compile.cmd
+++ backend/compile.cmd
@@ -3,7 +3,7 @@ mkdir target\dependency
 cd target\dependency
 jar xf ..\*.jar
 mkdir project
-xcopy /E /I BOOT-INF\lib project\lib
+xcopy /E /I BOOT-INF\lib project\libc
 xcopy /E /I META-INF project\META-INF
 xcopy /E /I BOOT-INF\classes project
-docker restart springbootapp
+docker restart backend
